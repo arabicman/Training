9433f9e74f5f0922d14b1f255fd63f73ef5bdb1b
arabicman <8345807@qq.com>
2021-03-25 09:39:50 -0700

    go

---
+++ src/leetcode/algorithm/Q222_CountCompleteTreeNodes/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.algorithm.Q222_CountCompleteTreeNodes;
+
+import leetcode.algorithm.TreeNode;
+
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return countNodes(root.left)+countNodes(root.right)+1;
+    }
+}
