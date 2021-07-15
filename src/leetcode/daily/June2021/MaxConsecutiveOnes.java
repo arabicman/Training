a34912d5a459ef9575e27c451c7f898acfe6d4a8
arabicman <8345807@qq.com>
2021-07-14 20:26:48 -0700

    Mac

---
+++ src/leetcode/daily/June2021/MaxConsecutiveOnes.java
@@ -0,0 +1,36 @@
+package leetcode.daily.June2021;
+
+import java.util.ArrayDeque;
+
+public class MaxConsecutiveOnes {
+    public int longestOnes(int[] nums, int k){
+        ArrayDeque<Integer> zeros = new ArrayDeque<>();
+        int curLen = 0, maxLen = 0;
+        int head = 0, pt = 0;
+        while(pt < nums.length){
+            if(nums[pt] == 1){
+                curLen = pt - head + 1;
+                maxLen = Math.max(curLen, maxLen);
+                pt ++;
+            } else {
+                if(k > 0){
+                    k --;
+                    zeros.offerLast(pt);
+                    curLen = pt - head + 1;
+                    maxLen = Math.max(curLen, maxLen);
+                    pt ++;
+                }else{
+                    if(zeros.isEmpty()){
+                        head = pt;
+                        pt++;
+                    }else{
+                        head = zeros.pollFirst() + 1;
+                        k ++;
+                    }
+                    }
+                }
+            }
+        }
+        return maxLen;
+    }
+}
